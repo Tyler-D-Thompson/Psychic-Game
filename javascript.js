e8fb40403d9b8f9f8bfc85e464480ffe5555f8bc
Tyler Thompson <tdtthompson1999@gmail.com>
2020-01-30 16:00:38 -0700

    turn in

--- javascript.js
+++ javascript.js
@@ -1,42 +1,47 @@
 var letters =["a","b","c"];
-
 var lettersGuessed =[];
-
-varlettersToGuess = null;
-
-var guessesLeft = 10;
-
+var lettersToGuess = null;
+var guessesLeft = 9;
 var win =0 ;
+var losses = 0;
 
-var losse = 0;
-
-var updateGuessesLeft = function() {
-    document.querySelector("#amount-of-guesses-left").innerHTML = guessesLeft;
+var updateguessesLeft = function() {
+    document.querySelector("#guessesLeft").innerHTML = guessesLeft;
 };
-
 var updatelettersToGuess = function() {
     lettersToGuess = letters[Math.floor(Math.random() * letters.length)];
 
 };
-
 var updatedGuessedLetters = function() {
-    document.querySelector("#guesses-so-far").innerHtML = lettersGuesses.join(", ");
+    document.querySelector("#lettersGuessed").innerHtML = lettersGuessed.join(",");
 };
-
 var reset = function() {
-    guessesLeft = 10;
+    guessesLeft = 9;
     lettersGuessed = [];
     updateGuessesLeft();
     updatedGuessedLetters();
-    updatelettersToGuess();
+    updateLettersToGuess();
 };
-
+updatelettersToGuess();
+updateguessesLeft();
 document.onkeydown = function(event) {
-    gueesesLeft--;
+    guessesLeft--;
+    var letter = event.key.toLowerCase();
+    lettersGuessed.push(letter);
+    updateguessesLeft();
+    updated(GuessedLetters);
+    if (letter === lettersToGuess) {
+        wins++;
+        document.querySelector("#win").innerHTML = win;
+        reset();
+    }
+    if (guessesLeft === 0) {
+        losses++;
+        document.querySelector("#losses").innerHTML = losses;
+        reset();
+    }
+    
 
 };
 
-var letter = event.key.toLowerCase();
-
-lettersGuessed.push(letter);
 
